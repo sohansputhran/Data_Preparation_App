f1208f9b148a1f6af4187226442cf92f4a424edc
Sohan Puthran <puthran.sohan@gmail.com>
2021-11-09 09:54:23 -0600

    Read CSV file

--- app.py
+++ app.py
@@ -18,16 +18,20 @@ st.write("**Made By: [Sohan Puthran](https://www.linkedin.com/in/sohansputhran/)
 # Input your csv
 st.sidebar.header('Upload your CSV data')
 uploaded_file = st.sidebar.file_uploader("", type=["csv"])
-st.sidebar.header('Or')
+
+data_selected = False
+if not data_selected:
+     st.sidebar.header('Or')
+     st.info('Awaiting for CSV file to be uploaded.')
 
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.write(df.head(5))
-
+     df = pd.read_csv(uploaded_file)
+     st.write(df.head(5))
+     data_selected = True
 else:
-    st.info('Awaiting for CSV file to be uploaded.')
-    if st.sidebar.button('Press to use Example Dataset'):
-        data = datasets.load_boston() 
-        df = pd.DataFrame(data.data, columns=data.feature_names)
-        st.markdown('The ""Boston Housing Prices** dataset is used as the example.')
-        st.write(df.head(5))
+     if st.sidebar.button('Press to use Example Dataset'):
+          data = datasets.fetch_california_housing() 
+          df = pd.DataFrame(data.data, columns=data.feature_names)
+          st.markdown('The **California Housing Prices** dataset is used as the example.')
+          st.write(df.head(5))
+          data_selected = True
