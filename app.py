99919312ed99faa4a1a7a13daf16e06848d96096
Sohan Puthran <puthran.sohan@gmail.com>
2021-11-13 00:25:50 -0600

    Fixed UI issues

--- app.py
+++ app.py
@@ -22,27 +22,25 @@ st.write("**Made By: [Sohan Puthran](https://www.linkedin.com/in/sohansputhran/)
 st.sidebar.header('Upload your CSV data')
 uploaded_file = st.sidebar.file_uploader("", type=["csv"])
 
-data_selected = False
-if not data_selected:
-     st.sidebar.header('Or')
-     st.info('Awaiting for CSV file to be uploaded.')
+st.sidebar.header('Or')
+button = st.sidebar.button('Press to use example dataset')
+
+st.write('*Please upload a CSV file in the sidebar or use an example dataset.*')
 
 if uploaded_file is not None:
      df = pd.read_csv(uploaded_file)
-     data_selected = True
      pr = ProfileReport(df, explorative=True)
      st.header('**Input DataFrame**')
-     st.write(df.head(5))
+     info = st.write(df.head(5))
      st.write('---')
      st.header('**Exploratory Data Analysis**')
      st_profile_report(pr)
 else:
-     if st.sidebar.button('Press to use Example Dataset'):
+     if button:
           data = datasets.fetch_california_housing() 
           df = pd.DataFrame(data.data, columns=data.feature_names)
           st.markdown('The **California Housing Prices** dataset is used as the example.')
           st.write(df.head(5))
-          data_selected = True
 
           pr = ProfileReport(df, explorative=True)
           st.write('---')
